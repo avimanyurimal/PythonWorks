654e1916c242ac54b8906398b25c8da98e8b4401
rimal <rimal.avimanyu@gmail.com>
2023-04-18 19:47:18 +0545

    python

--- practice/hahahahahahahahaaaaaaaaaaa.py
+++ practice/hahahahahahahahaaaaaaaaaaa.py
@@ -2,10 +2,9 @@ import random
 random = 0
 win = 0
 while True:
-    guess = input(
-        "guess the value of two dice(2-12)or enter 'q' to quite the game:")
+    guess = input("guess the value of two dice(2-12)or enter 'q' to quite the game:")
     if guess == "q":
-        break
+       break
     guess = int(guess)
     if guess < 2 or guess > 12:
         print("you have enter the invalid guess plesse enter the value between 2-12")
@@ -14,13 +13,12 @@ while True:
     roll = roll1+roll2
     if roll == guess:
         print("congratulation")
-        win += 1
+        win+= 1
     else:
-        print(sorry, roll)
-    random += 1
+        print("sorry,",roll)
+    random+= 1
 if round > 0:
     win_percent = 100*win/round
-    print(f"you played", round, "rounds and wqon",
-          win, "of them", win_percent, "%")
+    print(f"you played", round, "rounds and won", win, "of them", win_percent,"%")
 else:
     print("you didnt played another round")
